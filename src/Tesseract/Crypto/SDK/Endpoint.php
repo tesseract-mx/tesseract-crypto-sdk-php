928bb8e4724b8797ccfb212503866ed62a7dc5b4
Cristian Jaramillo <cristian_gerar@hotmail.com>
2019-01-10 17:08:59 -0600

    Update .gitignore

---
+++ src/Tesseract/Crypto/SDK/Endpoint.php
@@ -0,0 +1,57 @@
+<?php namespace Tesseract\Crypto\SDK;
+
+use phpDocumentor\Reflection\Types\Array_;
+
+/**
+ * Cristian Jaramillo (cristian_gerar@hotmail.com)
+ *
+ * Class Endpoint
+ * @package App\Client
+ */
+final class Endpoint
+{
+
+    const API = "/api/v2/";
+
+    const AUTH = Endpoint::API . 'auth';
+
+    const INSTITUTION = Endpoint::API . 'institution';
+
+    const LICENSES = Endpoint::INSTITUTION . '/licenses';
+
+    const LICENSE = Endpoint::LICENSES . '/' . PathParam::LICENSE_ID;
+
+    const TOKENS = Endpoint::LICENSE . '/tokens';
+
+    const TOKEN = Endpoint::TOKENS . '/' . PathParam::TOKEN_ID;
+
+    const ADMIN_INSTITUTIONS = Endpoint::API . 'institutions';
+
+    const ADMIN_INSTITUTION = Endpoint::ADMIN_INSTITUTIONS . '/' . PathParam::INTITUTION_ID;
+
+
+    /**
+     * @param string $endpoint
+     * @param array $params
+     * @param array $query
+     * @return string
+     */
+    public static function replace(string $endpoint, array $params = array(), array $query = array()) : string
+    {
+        foreach ($params as $param => $value)
+        {
+            $endpoint = str_replace($param, $value, $endpoint);
+        }
+
+        $queryPath = array();
+
+        foreach ($query as $key => $value)
+            array_push($queryPath, $key . "=" . $value);
+
+        if(count($queryPath) >= 1)
+            $endpoint .= "?" . implode("&", $queryPath);
+
+        return $endpoint;
+    }
+
+}
